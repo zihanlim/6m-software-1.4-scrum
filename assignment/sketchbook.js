f3ead9b74f5cc82f4bd69739bc904a2e0c155348
zihanlim <zihan0804@gmail.com>
2023-06-04 12:55:40 +0800

    update assignment

---
+++ assignment/sketchbook.js
@@ -0,0 +1,8 @@
+/*var myStr="FirstLine\n\theader\n\tcontents\n\tconclusion\n\rSecondLine\"\n\r'Thirdline'\b\f"
+
+console.log (myStr)*/
+
+var myNamelength=0
+var myName='limzihan'
+
+console.log(myName.length)
